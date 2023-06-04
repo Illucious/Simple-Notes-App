ed64e431c5e35e55b2588fdeef4989fc29b49e7f
Illucious <visheshgarg570@gmail.com>
2023-06-05 02:40:11 +0530

    added routes

---
+++ website/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from .views import views
+from .auth import auth
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config[
+        "SECRET KEY"
+    ] = "THIS IS THE SECret key"  # a secret key that has to be made for each project
+
+    app.register_blueprint(views, urlprefix="/")
+    app.register_blueprint(auth, urlprefix="/")
+
+    return app
