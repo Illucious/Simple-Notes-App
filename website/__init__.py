8460d2d4c4e6283dfa4094c5fb9a8c2645ff400e
Illucious <visheshgarg570@gmail.com>
2023-06-08 22:37:07 +0530

    added form logic to login and signup pages

--- website/__init__.py
+++ website/__init__.py
@@ -6,7 +6,7 @@ from .auth import auth
 def create_app():
     app = Flask(__name__)
     app.config[
-        "SECRET KEY"
+        "SECRET_KEY"
     ] = "THIS IS THE SECret key"  # a secret key that has to be made for each project
 
     app.register_blueprint(views, urlprefix="/")
--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 views = Blueprint("views", __name__)
 
@@ -8,11 +8,23 @@ def home():
     return render_template("home.html")
 
 
-@views.route("/login")
+@views.route("/login", methods=["GET", "POST"])
 def login():
     return render_template("login.html")
 
 
-@views.route("/signup")
+@views.route("/signup", methods=["GET", "POST"])
 def logout():
+    if request.method == "POST":
+        email = request.form.get("email")
+        firstName = request.form.get("firstName")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+        if len(password1) < 8:
+            flash("password must be atleast 8 characters", category="error")
+        elif password1 != password2:
+            flash("password don't match", category="error")
+        else:
+            flash("New account Created", category="success")
     return render_template("signup.html")
